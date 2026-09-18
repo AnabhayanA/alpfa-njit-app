aa560ffba3b5c514cf40f5e23d898552bcd193db
Anabhayan <aa3452@njit.edu>
2026-09-18 06:52:01 -0400

    Refine splash spacing and restore staged logo animation

--- components/ALPFALoadingScreen.tsx
+++ components/ALPFALoadingScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Animated, Image, StyleSheet, View, useWindowDimensions } from 'react-native';
+import { Animated, Easing, Image, StyleSheet, View, useWindowDimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import AnimatedALPFAMark from './AnimatedALPFAMark';
 import NJITInstituteText from './NJITInstituteText';
@@ -8,90 +8,147 @@ const FULL_LOGO = require('../assets/images/NJITalpfa logo.pdf (6).png');
 const NAVY = '#030712';
 const SPLASH_DURATION_MS = 5000;
 
-type Props = {
-  onAnimationComplete?: () => void;
-};
+type Props = { onAnimationComplete?: () => void };
 
 export default function ALPFALoadingScreen({ onAnimationComplete }: Props) {
   const { width, height } = useWindowDimensions();
   const insets = useSafeAreaInsets();
   const size = Math.min(width * 0.92, 390);
   const visibleHeight = Math.max(1, height - insets.top - insets.bottom);
-  const alpfaProgress = useRef(new Animated.Value(1)).current;
+
+  const alpfaProgress = useRef(new Animated.Value(0)).current;
+  const wordmarkOpacity = useRef(new Animated.Value(0)).current;
+  const njitOpacity = useRef(new Animated.Value(0)).current;
+  const njitTranslate = useRef(new Animated.Value(8)).current;
+  const groupScale = useRef(new Animated.Value(0.96)).current;
 
   useEffect(() => {
+    Animated.sequence([
+      Animated.timing(alpfaProgress, {
+        toValue: 1,
+        duration: 1200,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: false,
+      }),
+      Animated.parallel([
+        Animated.timing(wordmarkOpacity, {
+          toValue: 1,
+          duration: 450,
+          useNativeDriver: true,
+        }),
+        Animated.timing(njitOpacity, {
+          toValue: 1,
+          duration: 650,
+          useNativeDriver: true,
+        }),
+        Animated.timing(njitTranslate, {
+          toValue: 0,
+          duration: 650,
+          easing: Easing.out(Easing.cubic),
+          useNativeDriver: true,
+        }),
+        Animated.spring(groupScale, {
+          toValue: 1,
+          friction: 8,
+          tension: 45,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start();
+
     const timer = setTimeout(() => onAnimationComplete?.(), SPLASH_DURATION_MS);
     return () => clearTimeout(timer);
-  }, [onAnimationComplete]);
+  }, [alpfaProgress, groupScale, njitOpacity, njitTranslate, onAnimationComplete, wordmarkOpacity]);
 
   return (
     <View pointerEvents="auto" style={styles.root}>
-      <View
-        style={[
-          styles.centerStage,
-          { top: insets.top, height: visibleHeight },
-        ]}
-      >
-      <View style={[styles.lockup, { width: size, height: size }]}>
-        <View
-          pointerEvents="none"
+      <View style={[styles.centerStage, { top: insets.top, height: visibleHeight }]}>
+        <Animated.View
           style={[
-            styles.alpfaLayer,
+            styles.lockup,
             {
               width: size,
               height: size,
-              transform: [{ translateY: -size * 0.08 }],
+              transform: [{ scale: groupScale }],
             },
           ]}
         >
-          <AnimatedALPFAMark progress={alpfaProgress} size={size} />
           <View
+            pointerEvents="none"
             style={[
-              styles.alpfaWordmarkCrop,
+              styles.alpfaLayer,
               {
-                left: size * 0.20,
-                top: size * 0.43,
-                width: size * 0.60,
-                height: size * 0.10,
+                width: size,
+                height: size,
+                transform: [{ translateY: size * 0.015 }],
               },
             ]}
           >
-            <Image
-              source={FULL_LOGO}
-              resizeMode="contain"
-              style={{
-                width: size,
-                height: size,
-                marginLeft: -size * 0.20,
-                marginTop: -size * 0.43,
-              }}
-              accessibilityIgnoresInvertColors
-            />
+            <AnimatedALPFAMark progress={alpfaProgress} size={size} />
+            <Animated.View
+              style={[
+                styles.alpfaWordmarkCrop,
+                {
+                  left: size * 0.20,
+                  top: size * 0.43,
+                  width: size * 0.60,
+                  height: size * 0.10,
+                  opacity: wordmarkOpacity,
+                },
+              ]}
+            >
+              <Image
+                source={FULL_LOGO}
+                resizeMode="contain"
+                style={{
+                  width: size,
+                  height: size,
+                  marginLeft: -size * 0.20,
+                  marginTop: -size * 0.43,
+                }}
+                accessibilityIgnoresInvertColors
+              />
+            </Animated.View>
           </View>
-        </View>
-        <View
-          style={[
-            styles.highlanderCrop,
-            {
-              left: size * 0.20,
-              top: size * 0.56,
-              width: size * 0.18,
-              height: size * 0.18,
-            },
-          ]}
-        >
-          <Image
-            source={FULL_LOGO}
-            resizeMode="contain"
-            style={{ width: size, height: size, marginLeft: -size * 0.20, marginTop: -size * 0.56 }}
-            accessibilityIgnoresInvertColors
-          />
-        </View>
 
-        <View pointerEvents="none" style={StyleSheet.absoluteFillObject}>
-          <NJITInstituteText size={size} />
-        </View>
-      </View>
+          <Animated.View
+            style={[
+              styles.njitLayer,
+              {
+                opacity: njitOpacity,
+                transform: [{ translateY: njitTranslate }],
+              },
+            ]}
+          >
+            <View
+              style={[
+                styles.highlanderCrop,
+                {
+                  left: size * 0.20,
+                  top: size * 0.56,
+                  width: size * 0.18,
+                  height: size * 0.18,
+                },
+              ]}
+            >
+              <Image
+                source={FULL_LOGO}
+                resizeMode="contain"
+                style={{
+                  width: size,
+                  height: size,
+                  marginLeft: -size * 0.20,
+                  marginTop: -size * 0.56,
+                }}
+                accessibilityIgnoresInvertColors
+              />
+            </View>
+
+            <View pointerEvents="none" style={StyleSheet.absoluteFillObject}>
+              <NJITInstituteText size={size} />
+            </View>
+          </Animated.View>
+        </Animated.View>
       </View>
     </View>
   );
@@ -120,6 +177,9 @@ const styles = StyleSheet.create({
   alpfaWordmarkCrop: {
     position: 'absolute',
     overflow: 'hidden',
+  },
+  njitLayer: {
+    ...StyleSheet.absoluteFillObject,
   },
   highlanderCrop: {
     position: 'absolute',
