4967e08c1779aead535f875b986105d9daf10777
Anabhayan <aa3452@njit.edu>
2026-09-18 06:18:33 -0400

    Show only centered Highlander and white NJIT lockup on splash

--- components/ALPFALoadingScreen.tsx
+++ components/ALPFALoadingScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, useWindowDimensions } from 'react-native';
+import NJITInstituteText from './NJITInstituteText';
 
+const FULL_LOGO = require('../assets/images/NJITalpfa logo.pdf (6).png');
 const NAVY = '#030712';
 const SPLASH_DURATION_MS = 5000;
 
@@ -9,20 +11,56 @@ type Props = {
 };
 
 export default function ALPFALoadingScreen({ onAnimationComplete }: Props) {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      onAnimationComplete?.();
-    }, SPLASH_DURATION_MS);
+  const { width } = useWindowDimensions();
+  const size = Math.min(width * 0.92, 390);
 
+  useEffect(() => {
+    const timer = setTimeout(() => onAnimationComplete?.(), SPLASH_DURATION_MS);
     return () => clearTimeout(timer);
   }, [onAnimationComplete]);
 
-  return <View pointerEvents="auto" style={styles.root} />;
+  return (
+    <View pointerEvents="auto" style={styles.root}>
+      <View style={[styles.lockup, { width: size, height: size }]}>
+        <View
+          style={[
+            styles.highlanderCrop,
+            {
+              left: size * 0.20,
+              top: size * 0.56,
+              width: size * 0.18,
+              height: size * 0.18,
+            },
+          ]}
+        >
+          <Image
+            source={FULL_LOGO}
+            resizeMode="contain"
+            style={{ width: size, height: size, marginLeft: -size * 0.20, marginTop: -size * 0.56 }}
+            accessibilityIgnoresInvertColors
+          />
+        </View>
+
+        <View pointerEvents="none" style={StyleSheet.absoluteFillObject}>
+          <NJITInstituteText size={size} />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   root: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: NAVY,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lockup: {
+    position: 'relative',
+  },
+  highlanderCrop: {
+    position: 'absolute',
+    overflow: 'hidden',
   },
 });
