43fed5887b6f016a8593bae68d1b0b9c5c96f59a
Anabhayan <aa3452@njit.edu>
2026-09-26 12:35:58 -0400

    Remove unused dual camera native module

--- modules/alpfa-dual-camera/src/AlpfaDualCameraModule.web.ts
+++
@@ -1,6 +0,0 @@
-import { registerWebModule, NativeModule } from 'expo';
-
-// AlpfaDualCameraModule is not available on the web platform.
-class AlpfaDualCameraModule extends NativeModule<{}> {}
-
-export default registerWebModule(AlpfaDualCameraModule, 'AlpfaDualCameraModule');
