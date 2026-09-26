995342c9d3efd33154e7cdb7bb6e91ca62ec9e2b
Anabhayan <aa3452@njit.edu>
2026-09-26 12:36:02 -0400

    Remove unused dual camera native module

--- modules/alpfa-dual-camera/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AlpfaDualCameraModule } from './src/AlpfaDualCameraModule';
-export { default as AlpfaDualCameraView } from './src/AlpfaDualCameraView';
-export type { AlpfaDualCameraViewRef, DualCameraPhoto } from './src/AlpfaDualCamera.types';
